2775b2ba865c2c29b44c406654d01968d8e9f1fc
Patrick Hindmarsh <patrick@hindmar.sh>
2015-02-20 15:12:37 +1300

    Bump schema library

--- src/Validator.php
+++ src/Validator.php
@@ -4,18 +4,37 @@
 namespace Wave\Swagger\Generator;
 
 
+use JsonSchema\RefResolver;
+use JsonSchema\Uri\UriRetriever;
+
 class Validator {
 
     private $validation_schema;
 
-    public function __construct($validation_schema = null){
+    public function __construct($validation_schema_file = null){
 
-        $this->validation_schema = $validation_schema;
+        if(!file_exists($validation_schema_file))
+            throw new \RuntimeException("Validation schema {$validation_schema_file} not found");
+
+        $retreiver = new UriRetriever();
+        $resolver = new RefResolver($retreiver);
+
+        $schema = $retreiver->retrieve($validation_schema_file, 'file://');
+        $resolver->resolve($schema);
+
+        $this->validation_schema = $schema;
 
     }
 
     public function validate($swagger_schema){
 
+        $validator = new \JsonSchema\Validator();
+        $validator->check($swagger_schema, $this->validation_schema);
+
+        if(!$validator->isValid()){
+            return $validator->getErrors();
+        }
+        else return true;
 
     }
 
